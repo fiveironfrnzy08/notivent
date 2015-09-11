8510f9528c7d739dc7212ce61c94095a6cb458e0
Ryan Gray <fiveironfrnzy08@gmail.com>
2015-09-10 22:06:06 -0500

    Cronofy authorization

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,7 @@
 angular.module('notivent.controllers', [])
 
-	.controller('FeedController', ['$rootScope', '$scope', 'OAuth', '$ionicPlatform', 'WebSQL', '$q', '$timeout',
-		function ($rootScope, $scope, OAuth, $ionicPlatform, WebSQL, $q, $timeout) {
+	.controller('FeedController', ['$rootScope', '$scope', 'OAuth', '$ionicPlatform', 'WebSQL', '$q', '$cordovaOauthUtility', '$http',
+		function ($rootScope, $scope, OAuth, $ionicPlatform, WebSQL, $q, $cordovaOauthUtility, $http) {
 
 			spinner.show(null, "Loading calendar", true);
 
@@ -31,115 +31,195 @@ angular.module('notivent.controllers', [])
 				})
 			);
 			$q.all(promises).then(function(){
-				if(refreshToken && accessToken){
-					console.log('refresh');
-					$scope.authType = 'refresh';
-					$.oauth2({
-						auth_url: 'https://accounts.google.com/o/oauth2/auth',
-						token_url: 'https://accounts.google.com/o/oauth2/token',
-						response_type: 'code',
-						client_id: '843842939683-eg5k2v48mhdgbd0m3knhbcck9463q3l7.apps.googleusercontent.com',
-						grant_type: 'refresh_token',
-						client_secret: 't6vtYOdccrfWyZ6_FVIf9zAP',
-						refresh_token: refreshToken,
-						access_token: accessToken,
-						redirect_uri: 'http://localhost:8100/callback',
-						other_params: {scope: 'https://www.googleapis.com/auth/calendar'}
-					}, function (token, response) {
-						console.log('refresh: ' + token);
-						console.log(response);
-						$scope.handleAuth(token, response);
-					}, function (error, response) {
+				//if(refreshToken && accessToken){
+				//	console.log('refresh');
+				//	$scope.authType = 'refresh';
+				//	$.oauth2({
+				//		auth_url: 'https://accounts.google.com/o/oauth2/auth',
+				//		token_url: 'https://accounts.google.com/o/oauth2/token',
+				//		response_type: 'code',
+				//		client_id: '843842939683-eg5k2v48mhdgbd0m3knhbcck9463q3l7.apps.googleusercontent.com',
+				//		grant_type: 'refresh_token',
+				//		client_secret: 't6vtYOdccrfWyZ6_FVIf9zAP',
+				//		refresh_token: refreshToken,
+				//		access_token: accessToken,
+				//		redirect_uri: 'http://localhost:8100/callback',
+				//		other_params: {scope: 'https://www.googleapis.com/auth/calendar'}
+				//	}, function (token, response) {
+				//		console.log('refresh: ' + token);
+				//		console.log(response);
+				//		$scope.handleAuth(token, response);
+				//	}, function (error, response) {
+				//		console.warn(error);
+				//		console.warn(response);
+				//		$scope.error = error;
+				//		spinner.hide();
+				//	});
+				//} else {
+				//	console.log('initial');
+				//	$scope.authType = 'initial';
+				//	$.oauth2({
+				//		auth_url: 'https://app.cronofy.com/oauth/authorize',
+				//		response_type: 'code',
+				//		client_secret: 'cma6MbCKEZOBQhrPC8ubm1S5CYSLN_cFlg4uKiPpc76mPErT_DOZJ0pyfv42z5EV6OY1RU68PRfajuE-jbaohg',
+				//		token_url: 'https://app.cronofy.com/oauth/token',
+				//		client_id: '30PX-b-wrbocfhNevzNWBhEwI2TTkfda',
+				//		redirect_uri: 'http://localhost:8100/callback',
+				//		other_params: {scope: 'read_events list_calendars', access_type: 'offline'}
+				//	}, function (token, response) {
+				//		console.log('initial: ' + token);
+				//		console.log(response);
+				//		$scope.handleAuth(token, response);
+				//	}, function (error, response) {
+				//		console.warn(error);
+				//		console.warn(response);
+				//		$scope.error = error;
+				//		spinner.hide();
+				//	});
+				//}
+				cronofy(
+					'30PX-b-wrbocfhNevzNWBhEwI2TTkfda',
+					[
+						'read_events','list_calendars'
+					],
+					{
+						token_url: 'https://app.cronofy.com/oauth/token',
+						client_secret: 'cma6MbCKEZOBQhrPC8ubm1S5CYSLN_cFlg4uKiPpc76mPErT_DOZJ0pyfv42z5EV6OY1RU68PRfajuE-jbaohg',
+						redirect_uri: 'http://localhost:8100/callback'
+					}
+				).then(function (results) {
+						console.log('initial: ' + results.access_token);
+						console.log(JSON.stringify(results));
+						$scope.handleAuth(results);
+					}, function(error){
 						console.warn(error);
 						console.warn(response);
 						$scope.error = error;
 						spinner.hide();
-					});
-				} else {
-					console.log('initial');
-					$scope.authType = 'initial';
-					$.oauth2({
-						auth_url: 'https://accounts.google.com/o/oauth2/auth',
-						response_type: 'code',
-						client_secret: 't6vtYOdccrfWyZ6_FVIf9zAP',
-						token_url: 'https://accounts.google.com/o/oauth2/token',
-						client_id: '843842939683-eg5k2v48mhdgbd0m3knhbcck9463q3l7.apps.googleusercontent.com',
-						redirect_uri: 'http://localhost:8100/callback',
-						other_params: {scope: 'https://www.googleapis.com/auth/calendar', access_type: 'offline'}
-					}, function (token, response) {
-						console.log('initial: ' + token);
-						console.log(response);
-						$scope.handleAuth(token, response);
-					}, function (error, response) {
-						console.warn(error);
-						console.warn(response);
-						$scope.error = error;
-						spinner.hide();
-					});
-				}
+					}
+				);
 			});
 
-			var requestList = function (index) {
-
-				var params = {
-					'calendarId': $scope.cals[index].id,
-					'pageToken': $scope.cals[index].nextPageToken
-				};
-				if($scope.cals[index].syncToken){
-					params.syncToken = $scope.cals[index].syncToken;
-				}
-
-				var request = gapi.client.calendar.events.list(params);
-
-				try {
-					request.execute(function (response) {
-
-						$scope.cals[index].nextPageToken = response.nextPageToken;
-
-						$scope.cals[index].syncToken = response.nextSyncToken;
-						if($scope.cals[index].syncToken){
-							WebSQL.storeCal($scope.cals[index].id, $scope.cals[index].syncToken);
-						}
-
-						if(response.items){
-							if($scope.cals[index].items){
-								$.merge($scope.cals[index].items, response.items);
-							} else {
-								$scope.cals[index].items = response.items;
+			function cronofy(clientId, appScope, options) {
+				var deferred = $q.defer();
+				if(window.cordova) {
+					var cordovaMetadata = cordova.require("cordova/plugin_list").metadata;
+					if($cordovaOauthUtility.isInAppBrowserInstalled(cordovaMetadata) === true) {
+						var redirect_uri = "http://localhost/callback";
+						if(options !== undefined) {
+							if(options.hasOwnProperty("redirect_uri")) {
+								redirect_uri = options.redirect_uri;
 							}
 						}
-
-						if($scope.cals[index].nextPageToken){
-							requestList(index);
-						} else {
-							nextRequest(index);
-						}
-
-						$timeout(function () {
-							$scope.$apply();
+						var browserRef = window.open('https://app.cronofy.com/oauth/authorize?client_id=' + clientId + '&redirect_uri=' + redirect_uri + '&scope=' + appScope.join(" ") + '&response_type=code', '_blank', 'location=no,clearsessioncache=yes,clearcache=yes');
+						browserRef.addEventListener("loadstart", function(event) {
+							if((event.url).indexOf(redirect_uri) === 0) {
+								requestToken = (event.url).split("code=")[1];
+								var grantType = 'authorization_code';
+								$http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+								$http.defaults.headers.post.accept = 'application/json';
+								$http({method: "post", url: options.token_url, data: "client_id=" + clientId + "&client_secret=" + options.client_secret + "&redirect_uri=" + redirect_uri + "&code=" + requestToken + "&grant_type=" + grantType })
+									.success(function(data) {
+										if(data !== null && data !== undefined) {
+											browserRef.removeEventListener("exit",function(event){});
+											browserRef.close();
+											var callbackResponse = (event.url).split("#")[1];
+											var responseParameters = (callbackResponse).split("&");
+											var parameterMap = [];
+											for(var i = 0; i < responseParameters.length; i++) {
+												parameterMap[responseParameters[i].split("=")[0]] = responseParameters[i].split("=")[1];
+											}
+											if(data.access_token) {
+												deferred.resolve({ access_token: data.access_token, refresh_token: data.refresh_token, token_type: data.token_type, expires_in: data.expires_in });
+											} else {
+												deferred.reject("Problem authenticating");
+											}
+										}
+										deferred.resolve(data);
+									})
+									.error(function(data, status) {
+										deferred.reject("Problem authenticating");
+									})
+									.finally(function() {
+										setTimeout(function() {
+											browserRef.close();
+										}, 10);
+									});
+							}
 						});
-
-					}, function (error) {
-						nextRequest(index);
-						$scope.error = error;
-						spinner.hide();
-					});
-				} catch (e) {
-					nextRequest(index);
-					spinner.hide();
-					throw e;
-				}
-			};
-
-			function nextRequest(index){
-				if($scope.cals[index+1]){
-					requestList(index+1);
+						browserRef.addEventListener('exit', function(event) {
+							deferred.reject("The sign in flow was canceled");
+						});
+					} else {
+						deferred.reject("Could not find InAppBrowser plugin");
+					}
 				} else {
-					spinner.hide();
+					deferred.reject("Cannot authenticate via a web browser");
 				}
+				return deferred.promise;
 			}
 
-			$scope.handleAuth = function(token, response){
+			//var requestList = function (index) {
+			//
+			//	var params = {
+			//		'calendarId': $scope.cals[index].id,
+			//		'pageToken': $scope.cals[index].nextPageToken
+			//	};
+			//	if($scope.cals[index].syncToken){
+			//		params.syncToken = $scope.cals[index].syncToken;
+			//	}
+			//
+			//	var request = gapi.client.calendar.events.list(params);
+			//
+			//	try {
+			//		request.execute(function (response) {
+			//
+			//			$scope.cals[index].nextPageToken = response.nextPageToken;
+			//
+			//			$scope.cals[index].syncToken = response.nextSyncToken;
+			//			if($scope.cals[index].syncToken){
+			//				WebSQL.storeCal($scope.cals[index].id, $scope.cals[index].syncToken);
+			//			}
+			//
+			//			if(response.items){
+			//				if($scope.cals[index].items){
+			//					$.merge($scope.cals[index].items, response.items);
+			//				} else {
+			//					$scope.cals[index].items = response.items;
+			//				}
+			//			}
+			//
+			//			if($scope.cals[index].nextPageToken){
+			//				requestList(index);
+			//			} else {
+			//				nextRequest(index);
+			//			}
+			//
+			//			$timeout(function () {
+			//				$scope.$apply();
+			//			});
+			//
+			//		}, function (error) {
+			//			nextRequest(index);
+			//			$scope.error = error;
+			//			spinner.hide();
+			//		});
+			//	} catch (e) {
+			//		nextRequest(index);
+			//		spinner.hide();
+			//		throw e;
+			//	}
+			//};
+			//
+			//function nextRequest(index){
+			//	if($scope.cals[index+1]){
+			//		requestList(index+1);
+			//	} else {
+			//		spinner.hide();
+			//	}
+			//}
+			//
+			$scope.handleAuth = function(data){
 
 				var allCals = ['fiveironfrnzy08@gmail.com','isd282.org_q0ssoj8f02md7u5jd843amter0@group.calendar.google.com','apol5ib4uajkjs73m8fjtqq7cg@group.calendar.google.com'];
 				WebSQL.retrieveCals().then(function(cals){
@@ -154,21 +234,21 @@ angular.module('notivent.controllers', [])
 					}
 				});
 
-				OAuth.setAccessToken(token);
-				OAuth.setRefreshToken(response.refresh_token);
+				OAuth.setAccessToken(data.access_token);
+				OAuth.setRefreshToken(data.refresh_token);
 
-				gapi.client.setApiKey('AIzaSyAvO51RiSchOOpoYFBDhl6ITvUQpcM3Kd4');
-				gapi.client.load('calendar', 'v3', function(){
-
-					WebSQL.retrieveCals().then(function(cals){
-						$scope.cals = cals;
-						requestList(0);
-					});
-
-				}, function(error) {
-					$scope.error = error;
-					spinner.hide();
-				});
+				//gapi.client.setApiKey('AIzaSyAvO51RiSchOOpoYFBDhl6ITvUQpcM3Kd4');
+				//gapi.client.load('calendar', 'v3', function(){
+				//
+				//	WebSQL.retrieveCals().then(function(cals){
+				//		$scope.cals = cals;
+				//		requestList(0);
+				//	});
+				//
+				//}, function(error) {
+				//	$scope.error = error;
+				//	spinner.hide();
+				//});
 			};
 
 			$scope.isFeedShown = function(id){
